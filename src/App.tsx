06f4494422cb84ba047cc3e85b86de72a78f607d
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2022-04-30 14:13:33 +0200

    Replace default vite page with starting point for booking

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,63 @@
+import { useForm, UseFormRegisterReturn } from 'react-hook-form';
 import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((prevCount) => prevCount + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="flex flex-col w-screen h-screen bg-main-800 justify-center items-center">
+      <h1 className="font-bold text-xl m-4 text-white">
+          Book a train
+      </h1>
+      <Form />
     </div>
   );
 }
+
+const Form = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
+  return (
+    <form 
+      className="flex flex-col gap-3 min-w-[300px] w-4/12 justify-center items-center" 
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <DateInput
+        label="Departure" 
+        fieldConfig={register('departureDate', { required: true })} 
+        defaultVal={new Date()}
+      />
+      <button 
+        className="bg-secondary-200 w-4/12 mt-2 p-4 font-bold"
+        type="submit" 
+      >
+        Book
+      </button>
+    </form>
+  );
+};
+
+interface DateInputProps {
+  label: string;
+  fieldConfig: UseFormRegisterReturn;
+  disabled?: boolean;
+  defaultVal: Date;
+}
+const DateInput = ({ label, fieldConfig, disabled, defaultVal }: DateInputProps) => {
+  const [inputValue, setInputValue] = useState(defaultVal.toLocaleDateString('sv-SE'));
+
+  return (
+    <div className="w-full flex">
+      <span className="basis-1/3 bg-secondary-100 rounded-l-sm p-2 text-center">{label}</span>
+      <input 
+        className="basis-2/3 h-full rounded-r-sm px-2 disabled:bg-red-500" 
+        type="text" 
+        {...fieldConfig} 
+        disabled={disabled} 
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+    </div>
+  );
+};
 
 export default App;
