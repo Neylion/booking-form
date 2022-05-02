95b9998fb66961a2c730c5691a5f07e9b7dc416b
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2022-05-02 20:26:26 +0200

    Extract fieldconfig from form jsx

--- src/Pages/Booking/Form.tsx
+++ src/Pages/Booking/Form.tsx
@@ -5,6 +5,12 @@ import { dateStringHasValidFormat } from '../../utils/dates';
 import DateInput from './DateInput';
 import TypeInput from './TypeInput';
 
+const FIELD_NAMES = {
+  TICKET_TYPE: 'ticketType' as const,
+  DEPARTURE_DATE: 'departureDate' as const,
+  RETURN_DATE: 'returnDate' as const,
+};
+
 export interface SubmitData {
   ticketType: string;
   departureDate: string;
@@ -17,14 +23,46 @@ interface FormProps {
 const Form = ({ onSubmit }: FormProps) => {
   const { register, handleSubmit, trigger, getValues, setValue, formState: { errors } } = useForm<SubmitData>({ mode: 'onBlur', reValidateMode: 'onBlur' });
 
-  const ticketType = getValues('ticketType');
-  const departureDate = getValues('departureDate');
+  const ticketType = getValues(FIELD_NAMES.TICKET_TYPE);
+  const departureDate = getValues(FIELD_NAMES.DEPARTURE_DATE);
 
   useEffect(() => {
-    trigger('returnDate');
+    trigger(FIELD_NAMES.RETURN_DATE);
   }, [ticketType, departureDate, trigger]);
 
   const hasErrors = Object.keys(errors).length > 0;
+
+  const fieldConfigs = {
+    ticketType: register(FIELD_NAMES.TICKET_TYPE, { 
+      required: true,
+      validate: (inputValue) => {
+        if (!Object.values(TICKET_TYPES).includes(inputValue)) return `Must be one of: ${Object.values(TICKET_TYPES).join(', ')}`;
+        return true;
+      },
+    }),
+    departureDate: register(FIELD_NAMES.DEPARTURE_DATE, {
+      required: true,
+      validate: (inputValue) => {
+        if (!dateStringHasValidFormat(inputValue)) return ERROR_MESSAGES.INVALID_DATE;
+        const date = new Date(inputValue);
+        const todaysDate = new Date(new Date().setHours(0, 0, 0, 0));
+        if (date < todaysDate) return ERROR_MESSAGES.DATE_IN_PAST;
+        trigger(FIELD_NAMES.RETURN_DATE); // Re-evaluate the return date since its validation depends on this field
+        return true;
+      }, 
+    }),
+    returnDate: register(FIELD_NAMES.RETURN_DATE, { 
+      validate: (inputValue) => {
+        const requireReturnDate = ticketType === TICKET_TYPES.TWO_WAY;
+        if (!requireReturnDate) return true;
+        if (!dateStringHasValidFormat(inputValue)) return ERROR_MESSAGES.INVALID_DATE;
+        const date = new Date(inputValue);
+        if (departureDate && date < new Date(departureDate)) return ERROR_MESSAGES.RETURN_BEFORE_DEPARTURE;
+        return true;
+      }, 
+      disabled: getValues(FIELD_NAMES.TICKET_TYPE) === TICKET_TYPES.ONE_WAY,
+    }),
+  };
 
   return (
     <form 
@@ -33,46 +71,20 @@ const Form = ({ onSubmit }: FormProps) => {
     >
       <TypeInput 
         label="Type" 
-        fieldConfig={register('ticketType', { 
-          required: true,
-          validate: (inputValue) => {
-            if (!Object.values(TICKET_TYPES).includes(inputValue)) return `Must be one of: ${Object.values(TICKET_TYPES).join(', ')}`;
-            return true;
-          },
-        })} 
+        fieldConfig={fieldConfigs.ticketType}
         value={ticketType || TICKET_TYPES.ONE_WAY}
-        setValue={(value) => setValue('ticketType', value, { shouldValidate: true })}
+        setValue={(value) => setValue(FIELD_NAMES.TICKET_TYPE, value, { shouldValidate: true })}
         errors={errors}
       />
       <DateInput
         label="Departure" 
-        fieldConfig={register('departureDate', { 
-          required: true,
-          validate: (inputValue) => {
-            if (!dateStringHasValidFormat(inputValue)) return ERROR_MESSAGES.INVALID_DATE;
-            const date = new Date(inputValue);
-            const todaysDate = new Date(new Date().setHours(0, 0, 0, 0));
-            if (date < todaysDate) return ERROR_MESSAGES.DATE_IN_PAST;
-            trigger('returnDate'); // Re-evaluate the return date since its validation depends on this field
-            return true;
-          }, 
-        })} 
+        fieldConfig={fieldConfigs.departureDate} 
         defaultVal={new Date()}
         errors={errors}
       />
       <DateInput
         label="Return" 
-        fieldConfig={register('returnDate', { 
-          validate: (inputValue) => {
-            const requireReturnDate = ticketType === TICKET_TYPES.TWO_WAY;
-            if (!requireReturnDate) return true;
-            if (!dateStringHasValidFormat(inputValue)) return ERROR_MESSAGES.INVALID_DATE;
-            const date = new Date(inputValue);
-            if (departureDate && date < new Date(departureDate)) return ERROR_MESSAGES.RETURN_BEFORE_DEPARTURE;
-            return true;
-          }, 
-          disabled: getValues('ticketType') === TICKET_TYPES.ONE_WAY,
-        })} 
+        fieldConfig={fieldConfigs.returnDate} 
         defaultVal={new Date()}
         errors={errors}
       />
