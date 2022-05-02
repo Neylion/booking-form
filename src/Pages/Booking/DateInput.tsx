a5d6ffef62aafa2a61a4fed34b5c7e81816e28b8
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2022-05-02 20:39:42 +0200

    Use better error message when date field lacks value

--- src/Pages/Booking/DateInput.tsx
+++ src/Pages/Booking/DateInput.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { UseFormRegisterReturn } from 'react-hook-form';
+import { ERROR_MESSAGES } from '../../constants';
 import InputLayout from './InputLayout';
 
 interface DateInputProps {
@@ -11,9 +12,11 @@ interface DateInputProps {
 const DateInput = ({ label, fieldConfig, defaultVal, errors }: DateInputProps) => {
   const [inputValue, setInputValue] = useState(defaultVal.toLocaleDateString('sv-SE'));
 
-  const error = errors?.[fieldConfig.name]?.message || (!inputValue && 'Required');
+  const error = errors?.[fieldConfig.name];
+  const errorMessage = error && (error.message || ERROR_MESSAGES.INVALID_DATE);
+
   return (
-    <InputLayout label={label} error={error} disabled={fieldConfig.disabled}>
+    <InputLayout label={label} error={errorMessage} disabled={fieldConfig.disabled}>
       <input
         className="basis-2/3 h-full rounded-r-sm px-2 bg-inherit disabled:cursor-not-allowed" 
         type="text"
