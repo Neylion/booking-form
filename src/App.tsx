a22f3e34556b64d650fac678e9fe8688305c8668
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2022-05-02 20:34:28 +0200

    Move background color to html to prevent white scroll bounce

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import BookingPage from './Pages/Booking/BookingPage';
 function App() {
   // TODO Add potential page layout, routing, context, etc here
   return (
-    <div className="w-screen h-screen bg-main-800">
+    <div className="w-screen h-screen overflow-hidden">
       <BookingPage />;
     </div>
   );
